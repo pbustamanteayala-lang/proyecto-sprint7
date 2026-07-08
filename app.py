9f501680f43d31645e7f81f58a31c1b61f21db93
pbustamanteayala-lang <p.bustamanteayala@gmail.com>
2026-07-07 18:02:27 -0600

    ruta

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly as py
 
-car_data = pd.read_csv('C:\\Users\\STAFF11\\Visual studio code\\proyecto-sprint7\\vehicles_us.csv')
+car_data = pd.read_csv('vehicles_us.csv')
 df = pd.DataFrame(car_data)
 
 st.header('Vehicles data')
@@ -25,4 +25,21 @@ if hist_button:
  
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+# Ahora vamos a hacer un boton para grafico de dispersion 
+disp_button = st.button('Construir gráfico de dispersión')
+
+if disp_button:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un gráfico de dispersión utilizando plotly.graph_objects
+    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     st.plotly_chart(fig, use_container_width=True)
